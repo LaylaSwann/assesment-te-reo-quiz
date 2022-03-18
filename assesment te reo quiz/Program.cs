e0740a3ebb0f5a6149e31613c38137dbc8f2a1f9
Layla Swan <ac117431@avcol.school.nz>
2022-03-18 14:12:28 +1300

    Add project files.

---
+++ assesment te reo quiz/Program.cs
@@ -0,0 +1,48 @@
+﻿/* (this program will quiz you on the Te Reo Maori language)
+ * 
+ * 
+ * 
+ * 
+ */
+int
+Console.WriteLine("This program will quiz you on the Te Reo Maori Language.");
+Console.WriteLine("what does the word... translate to in english ?");
+Console.WriteLine("A.");
+Console.WriteLine("B.");
+Console.WriteLine("C.");
+Console.WriteLine("D.");
+switch ()
+{
+    case "A":
+        Console.WriteLine();
+            break;
+    case "B":
+      Console.WriteLine();
+        break;
+    case "c":
+        Console.WriteLine();
+        break;
+    case "D":
+        Console.WriteLine();
+        break;
+}
+Console.WriteLine("what does the word... translate to in english ?");
+Console.WriteLine("A.");
+Console.WriteLine("B.");
+Console.WriteLine("C.");
+Console.WriteLine("D.");
+switch ()
+{
+    case "A":
+        Console.WriteLine();
+        break;
+    case "B":
+        Console.WriteLine();
+        break;
+    case "c":
+        Console.WriteLine();
+        break;
+    case "D":
+        Console.WriteLine();
+        break;
+}
